4e7e0903b7eed89d2915713f515ad83bf6b34691
AdvayMonga <advaymonga@gmail.com>
2024-05-16 17:44:35 -0700

    Update proteinpred.py

--- proteinpred.py
+++ proteinpred.py
@@ -6,21 +6,26 @@ from modal import Image
 
 app = modal.App()
 
-image = (
+process_image = (
     Image.debian_slim(python_version="3.10")
     .apt_install("git")
+    .apt_install("wget")
     .pip_install("torch==1.5.1")
     .pip_install("biopython>1.78")
+    .pip_install("modal")
     .run_commands("git clone https://github.com/psipred/s4pred")
+    .run_commands("cd s4pred")
+    .run_commands("wget https://raw.githubusercontent.com/AdvayMonga/Protein/main/proteinpred.py")
 )
 
-def process_batch(batch):
-    results = {}
-    for seq_key,seq in batch:
-        print(f"processing {seq_key}")
-        output = subprocess.check_output(["python","run_model.py", seq], shell=False)
-        results[seq_key] = output
-    return results
+def process_batch(batch, output_dir):
+    seq_path = os.path.join(output_dir,"seqfile")
+    with open(seq_path,"a") as seqfile:    
+        for seq_key,seq in batch:
+           print(f"processing {seq_key}")
+           seqfile.write(f">{seq_key}\n{seq}\n")
+    subprocess.run(["python","run_model.py", "--save-files", "--silent", "--outdir", output_dir, seq_path], shell=False)
+    os.remove(seq_path)
 
 def readfile(filename):
     data = []
@@ -29,12 +34,6 @@ def readfile(filename):
             data.append(line.strip().split("\t"))
     return data
 
-
-
-def append_output(filename, results):
-    with open(filename, "a") as outfile:
-        for key, value in results.items():
-            outfile.write(f"{key}\t{value}\n")
 
 
 
@@ -53,48 +52,59 @@ def create_batches(data, num_workers):
         start = end
     return batches
 
-def process_allbatches(batches, outfile):
-    for batch in batches:
-        results = process_batch(batch)
-        append_output(outfile, results)
-
-@app.function()
+@app.function(image=process_image)
 def myprocess(filename, start, end):
+    outdir = f"outdir_{start}"
+    os.mkdir(outdir)
     data = readfile(filename)
     mybatch = data[start:end]
-    results = process_batch(mybatch)
-    outfile = f"outfile_{start}"
-    append_output(outfile, results)
-    return outfile
+    process_batch(mybatch,outdir)
+    return outdir
 
-
+#Output of each batch goes into separate directory
+#in myprocess, create the new directory and return the directory location
+#the directory is passed into process batch
+#combine files -> gets files from each directory and combines them into one
 
     
 def combine_files(infiles, outfilename):
+    print(f"infiles: {infiles}, outfilename: {outfilename}")
     with open(outfilename, "a") as outfile:
         for file in infiles:
+            filename = os.path.splitext(os.path.basename(file))[0]
+            outfile.write(f"\n{filename}\n")
             with open(file, "r") as input:
                 for line in input:
                     outfile.write(line)
             os.remove(file)
+
+def combine_directory(indirs, outfilename):
+    print(f"combine_director: indirs: {indirs}, outfile: {outfilename}")
+    for dir in indirs:
+        dirfiles = os.listdir(dir)
+        dirpaths = []
+        for file in dirfiles:
+            dirpath = os.path.join(dir, file)
+            dirpaths.append(dirpath)
+        combine_files(dirpaths, outfilename)
+        os.rmdir(dir)
+
+
+            
 
 
 @app.local_entrypoint()
 def main(datafile: str, resultsfile: str, num_workers: int):
     data = readfile(datafile)
     batches = create_batches(data, num_workers)
-    allpredfiles = []
+    allpreddirs = []
     for batch in batches:
-        predfile = myprocess.local(datafile, batch[0], batch[1])
-        allpredfiles.append(predfile)
-    combine_files(allpredfiles, resultsfile)
+        preddir = myprocess.local(datafile, batch[0], batch[1])
+        allpreddirs.append(preddir)
+    combine_directory(allpreddirs, resultsfile)
 
 if __name__ == "__main__":
     datafile = "test10.txt"
     resultsfile = "results"
-    num_workers = 10
+    num_workers = 5
     main(datafile, resultsfile, num_workers)
-
-        
-
-        
